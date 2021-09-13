6f48484616a5a9b01e3c06dff1aa15c697a4774d
Lyre Chain Developers <mail@lyrechain.ch>
2021-09-13 16:56:17 +0200

    Intial setup

---
+++ Contract/src/lib.rs
@@ -0,0 +1,107 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+/// Define the type of state stored in accounts
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct Blake3ByteHashFromClient {
+    /// number of greetings
+    pub hash: [u8; 32],
+}
+
+// Declare and export the program's entrypoint
+entrypoint!(process_instruction);
+
+// Program entrypoint's implementation
+pub fn process_instruction(
+    program_id: &Pubkey, // Public key of the account the hello world program was loaded into
+    accounts: &[AccountInfo], // The account to say hello to
+    _instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
+) -> ProgramResult {
+    msg!("Received Blake3 Hashed Agreements [Account1, Account2, Blake3_Byte_Hash]");
+
+    // Iterating accounts is safer then indexing
+    let accounts_iter = &mut accounts.iter();
+
+    // Get the account to say hello to
+    let account = next_account_info(accounts_iter)?;
+
+    // The account must be owned by the program in order to modify its data
+    if account.owner != program_id {
+        msg!("Wrong program ID provided by the account");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    let cloned_data = &*account.data.borrow();
+    let hex_encoded_blake3hash = match core::str::from_utf8(cloned_data) {
+        Ok(hash) => hash.clone(),
+        Err(_) => {
+            msg!("Could not decode the hash data. Check the data you sent if it is correct");
+            return Err(ProgramError::InvalidAccountData);
+        }
+    };
+
+    msg!(
+        "The parties agreement is binded by: {}",
+        hex_encoded_blake3hash
+    );
+
+    Ok(())
+}
+
+// Sanity tests
+#[cfg(test)]
+mod test {
+    use super::*;
+    use solana_program::clock::Epoch;
+    use std::mem;
+
+    #[test]
+    fn test_sanity() {
+        let program_id = Pubkey::default();
+        let key = Pubkey::default();
+        let mut lamports = 0;
+        let mut data = vec![0; mem::size_of::<u32>()];
+        let owner = Pubkey::default();
+        let account = AccountInfo::new(
+            &key,
+            false,
+            true,
+            &mut lamports,
+            &mut data,
+            &owner,
+            false,
+            Epoch::default(),
+        );
+        let instruction_data: Vec<u8> = Vec::new();
+
+        let accounts = vec![account];
+
+        assert_eq!(
+            GreetingAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            0
+        );
+        process_instruction(&program_id, &accounts, &instruction_data).unwrap();
+        assert_eq!(
+            GreetingAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            1
+        );
+        process_instruction(&program_id, &accounts, &instruction_data).unwrap();
+        assert_eq!(
+            GreetingAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            2
+        );
+    }
+}
